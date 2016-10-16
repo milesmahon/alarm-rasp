4eca04e2074b42055d001428d2cd4b21d11c9f78
miles <milesmahon@protonmail.com>
2016-10-15 23:27:05 -0400

    refactor alarmMaster

--- alarm/alarmMaster.py
+++ alarm/alarmMaster.py
@@ -3,37 +3,40 @@ import time
 import subprocess
 import ConfigParser
 Config=ConfigParser.ConfigParser()
+main()
 
-try:
-	Config.read('alarm.config')
-except:
-	raise Exception("Couldn't connect to alarm config file")
+def main():
+	try:
+		Config.read('alarm.config')
+	except:
+		raise Exception("Couldn't connect to alarm config file")
 
-db = int(Config.get('main','debug'))
+	db = int(Config.get('main','debug'))
 
-if (db==1):
-	print (time.strftime("%d/%m/%Y"))
-	print (time.strftime("%I:%M:%S"))
-	print ("Alarm start\n\n")
+	if (db==1):
+		print (time.strftime("%d/%m/%Y"))
+		print (time.strftime("%I:%M:%S"))
+		print ("Alarm start\n\n")
 
-#uses list of all programs listed in alarm.config
-progList = Config.sections()
-progList.pop(0) #pops "main" because we won't use it here
+	#uses list of all programs listed in alarm.config
+	progList = Config.sections()
+	progList.pop(0) #pops "main" because we won't use it here
+	run(progList)
 
 """Start running progs ---------------------------------------"""
 
-#subprocess.call("./speech.sh Good morning. ")
+def run(progList):
+	#subprocess.call("./speech.sh Good morning. ")
+	#all files have name format *respective section*Get.py, eg weatherGet, newsGet...
+	for x in progList:
+		try:
+			subprocess.call("python " + x + "Get.py", shell=True)
+		except:
+			raise Exception("Couldn't retrieve " + x)
 
-#all files have name format *respective section*Get.py, eg weatherGet, newsGet...
-for x in progList:
-	try:
-		subprocess.call("python " + x + "Get.py", shell=True)
-	except:
-		raise Exception("Couldn't retrieve " + x)
+	#alternate: call each program to get their return value ( could edit to be a string ), parse it here in this file
+	#and send each short sentence to speech.sh so it all flows smoothly and stops at all (". ")'s
 
-#alternate: call each program to get their return value ( could edit to be a string ), parse it here in this file
-#and send each short sentence to speech.sh so it all flows smoothly and stops at all (". ")'s
-
-if (db == 1):
-	print progList
-	print ("Exited successfully\n\n")
+	if (db == 1):
+		print progList
+		print ("Exited successfully\n\n")
