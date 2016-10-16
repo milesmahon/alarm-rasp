681456048a24d97c6d23ce82e3eef71aa9539f1f
miles <mmahon382@gmail.com>
2016-10-15 23:20:40 -0400

    refactor news & weather

--- alarm/newsGet.py
+++ alarm/newsGet.py
@@ -1,41 +1,36 @@
 # -*- coding: utf-8 -*-
 import feedparser #for parsing rss feeds
 import subprocess
-import textwrap
+#import textwrap
 import ConfigParser
 
 Config=ConfigParser.ConfigParser()
 Config.read('alarm.config')
+db=int(Config.get('main','debug'))
+main()
 
-"""Variables--------------------------------------"""
+def main():
+	localFeed = Config.get('news','newsSourceURL')
 
-localFeed = Config.get('news','newsSourceURL')
+	rss = feedparser.parse(localFeed)
+	newsTitle = Config.get('news','newsSourceTitle')
 
-rss = feedparser.parse(localFeed)
-newsTitle = Config.get('news','newsSourceTitle')
+	count = int(Config.get('news','numArticles'))
+	#config seems to assume parameters are strings, so force int
 
-count = int(Config.get('news','numArticles')) 
-#config seems to assume parameters are strings, so force int
+	if (db==1):
+		print count + " articles"
+		print "from " + localFeed + "\n"
+	speak(rss, count, newsTitle)
 
-db=int(Config.get('main','debug')) 
+def speak(rss, count, newsTitle):
+	news = "Now the news from " + newsTitle + "... "
+	for i in range(count):
+		news = news + rss.entries[i]["title"] + ". "
 
-if (db==1):
-	print count + " articles"
-	print "from " + localFeed + "\n"
+	news = news.encode('utf-8') #otherwise TTS doesn't understand
 
-"""Code--------------------------------------------"""
-
-news = "Now the news from " + newsTitle + "... "
-
-for i in range(count):
-	news = news + rss.entries[i]["title"] + ". "
-
-news = news.encode('utf-8') #otherwise TTS doesn't understand
-
-if (db==1):
-        print news
-
-subprocess.call("./speech.sh \"" + news + "\"", shell=True)
+	if (db==1):
+	        print news
+	subprocess.call("./speech.sh \"" + news + "\"", shell=True)
 #calls TTS script with "news" string from command line
-
-
--- alarm/weatherGet.py
+++ alarm/weatherGet.py
@@ -9,51 +9,49 @@ import ConfigParser
 Config=ConfigParser.ConfigParser()
 Config.read('alarm.config')
 localCity = Config.get('weather','city')
+main()
 
-#Getting weather info -----------------------
-weather_api = urllib.urlopen("http://api.openweathermap.org/data/2.5/weather?q=" + localCity + "&units=imperial")
-#can also be done with yahoo weather api
+def main():
+	#Getting weather info -----------------------
+	weather_api = urllib.urlopen("http://api.openweathermap.org/data/2.5/weather?q=" + localCity + "&units=imperial")
+	#can also be done with yahoo weather api
+	weatherFile = json.loads(weather_api.read())
+	#convert to JSON readable
 
-weatherFile = json.loads(weather_api.read())
-#convert to JSON readable
+	temp = weatherFile["main"]["temp"]
+	humid = weatherFile["main"]["humidity"]
+	wind = weatherFile["wind"]["speed"]
+	#snow and rain to be added
 
-temp = weatherFile["main"]["temp"]
-humid = weatherFile["main"]["humidity"]
-wind = weatherFile["wind"]["speed"]
-#snow and rain to be added
+	#Format weather string -------------------------------
+	temp = ("Temperature is " + str(temp)+ " degrees. ")
+	humid = (str(humid) + " percent humidity. ")
+	wind = ("Wind speed of " + str(wind) + " miles per hour. ")
 
-#Format weather string -------------------------------
-temp = ("Temperature is " + str(temp)+ " degrees. ")
-humid = (str(humid) + " percent humidity. ")
-wind = ("Wind speed of " + str(wind) + " miles per hour. ")
+	if (int(Config.get('main','debug')) == 1):
+		print temp
+		print humid
+		print wind
 
-if (int(Config.get('main','debug')) == 1):
-	print temp
-	print humid
-	print wind
-	
-if (int(Config.get('weather','humid')) != 1):
-	humid = ""
-	
-"""if (int(Config.get('weather','precip')) != 1):
-	precip = ""
-need to implement precip above	
-"""
+	if (int(Config.get('weather','humid')) != 1):
+		humid = ""
 
-if (int(Config.get('weather','wind')) != 1):
-	wind = ""
+	"""if (int(Config.get('weather','precip')) != 1):
+		precip = ""
+	need to implement precip above
+	"""
 
-weatherWords = "Today, " + temp + humid + wind
+	if (int(Config.get('weather','wind')) != 1):
+		wind = ""
 
+	weatherWords = "Today, " + temp + humid + wind
 
-#Speech with google translate ---------------------------
+	#Speech with google translate ---------------------------
 
-send = ("./speech.sh " + str(weatherWords))
-#Using text to speech script (speech.sh) by Dan Fountain, runs through google and
-#already accounts for 100 char limit in google voice. genius
+	send = ("./speech.sh " + str(weatherWords))
+	#Using text to speech script (speech.sh) by Dan Fountain, runs through google and
+	#already accounts for 100 char limit in google voice. genius
 
-subprocess.call(send, shell=True)
+	subprocess.call(send, shell=True)
 
 #works as of 7/21/15
-
-
